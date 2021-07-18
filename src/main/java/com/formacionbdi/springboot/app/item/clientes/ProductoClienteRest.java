74dc413b353638a534f250e83c919252d1a8ceba
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-18 17:51:22 -0300

    creacion ProductoClienteRest con configuracion Feign- @FeignClient posee nombre de servicio a consumir-app.poroperties- y su url- como tambien los GetMapping donde va los mismos endpoints que el proyecto original de Productos

--- src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
+++ src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -1,2 +1,19 @@
-package com.formacionbdi.springboot.app.item.clientes;public interface ProductoClienteRest {
+package com.formacionbdi.springboot.app.item.clientes;
+
+import com.formacionbdi.springboot.app.item.models.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name="servicio-productos", url="localost:8081")
+public interface ProductoClienteRest {
+
+    @GetMapping("/listar")
+    public List<Producto> listar();
+
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id);
 }
