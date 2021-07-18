f659f2e6c1724506e40c366f352fa10fa0680b1d
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-18 17:53:12 -0300

    clase que implementa la interfaz nueva Feign

---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
@@ -0,0 +1,26 @@
+package com.formacionbdi.springboot.app.item.models.service;
+
+import com.formacionbdi.springboot.app.item.clientes.ProductoClienteRest;
+import com.formacionbdi.springboot.app.item.models.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ItemServiceFeign implements ItemService {
+
+    @Autowired
+    private ProductoClienteRest clienteFeign;
+
+    @Override
+    public List<Item> findAll() {
+        return clienteFeign.listar().stream().map(producto -> new Item(producto,1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer cantidad) {
+        return new Item(clienteFeign.detalle(id),cantidad);
+    }
+}
