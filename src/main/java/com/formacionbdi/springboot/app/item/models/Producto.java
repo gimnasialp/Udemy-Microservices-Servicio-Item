ba78b5a692327a213633942a7a2f943d21c51cc7
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-20 20:26:51 -0300

    cambios Ribbon

--- src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -11,6 +11,8 @@ public class Producto {
 
     private Date createAt;
 
+    private Integer port;
+
     public Long getId() {
         return id;
     }
@@ -41,5 +43,13 @@ public class Producto {
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
     }
 }
--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Service("serviceRestTemplate")
+@Service
 public class ItemServiceImpl implements  ItemService{
 
 
