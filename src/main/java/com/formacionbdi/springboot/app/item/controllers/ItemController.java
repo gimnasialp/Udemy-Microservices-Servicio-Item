4da2dcbabe0448323d2f9dfcbf3033005d8045e7
migue_centellas_leon <migue.centellas@gmail.com>
2021-07-25 22:03:16 -0300

    25 - Tolerancia a fallos y latencia con Hystrix 2- Circuit Breaker Pattern

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +24,21 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
         return itemService.findById(id, cantidad);
     }
+
+    public Item metodoAlternativo(@PathVariable Long id, @PathVariable Integer cantidad){
+        Item item = new Item();
+        Producto producto =  new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return  item;
+    }
+
 }
